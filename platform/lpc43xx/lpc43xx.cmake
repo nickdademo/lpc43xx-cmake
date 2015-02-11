88c31764127951bd7efd3377a9a0324ebc2759de
Nick D'Ademo <nickdademo@gmail.com>
2015-02-11 22:52:12 +1100

    Initial commit.

---
+++ platform/lpc43xx/lpc43xx.cmake
@@ -0,0 +1,34 @@
+add_definitions(
+  -DCORE_M4
+  -D__USE_LPCOPEN
+  -D__NEWLIB__
+  -D__CODE_RED
+
+  -Wall                         # Enables all compiler warning messages
+
+  -c                            # Compiles source files without linking
+
+  -fmessage-length=0            # No line-wrapping for error messages (each message appears on a single line)
+  -ffunction-sections           # Place each function into its own section in the output file
+  -fdata-sections               # Place each data item into its own section in the output file
+  -fsingle-precision-constant   # Treat floating point constant as single precision constant instead of implicitly converting it to double precision constant
+  -fno-builtin                  # Don't recognize built-in functions that do not begin with `__builtin_' as prefix
+
+  -mcpu=cortex-m4
+  -mthumb
+  -mfpu=fpv4-sp-d16
+  -mfloat-abi=softfp
+)
+
+if(DEFINED CMAKE_RELEASE)
+  add_definitions(-O2 -Os)          # O2: Optimize even more, Os: Optimize for size
+else()
+  add_definitions(-O0 -g3 -DDEBUG)  # O0: Reduce compilation time and make debugging produce the expected results, g3: Level 3 debugging information
+endif()
+
+set(LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/platform/lpc43xx/lpc43xx.ld")
+set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -Xlinker --gc-sections -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mthumb -T ${LINKER_SCRIPT}")
+
+set(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)
+set(OUTPUT_NAME ${CMAKE_PROJECT_NAME}.axf)
+set(FULL_OUTPUT_NAME ${EXECUTABLE_OUTPUT_PATH}/${OUTPUT_NAME})
---
+++ platform/toolchain-gcc-arm-embedded.cmake
@@ -0,0 +1,43 @@
+include(CMakeForceCompiler)
+
+if(NOT TOOLCHAIN_PREFIX)
+  message(FATAL_ERROR "No TOOLCHAIN_PREFIX specified.")
+endif()
+
+set(TARGET_TRIPLET "arm-none-eabi")
+
+set(TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
+set(TOOLCHAIN_INC_DIR ${TOOLCHAIN_PREFIX}/${TARGET_TRIPLET}/include)
+set(TOOLCHAIN_LIB_DIR ${TOOLCHAIN_PREFIX}/${TARGET_TRIPLET}/lib)
+
+set(CMAKE_SYSTEM_NAME       Generic)
+set(CMAKE_SYSTEM_VERSION    1)
+set(CMAKE_SYSTEM_PROCESSOR  arm)
+
+if(WIN32)
+  set(TOOLCHAIN_BIN_SUFFIX ".exe")
+endif()
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-gcc${TOOLCHAIN_BIN_SUFFIX} CACHE INTERNAL "c compiler")
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-g++${TOOLCHAIN_BIN_SUFFIX} CACHE INTERNAL "cxx compiler")
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-as${TOOLCHAIN_BIN_SUFFIX} CACHE INTERNAL "asm compiler")
+
+set(CMAKE_OBJCOPY ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-objcopy${TOOLCHAIN_BIN_SUFFIX} CACHE INTERNAL "objcopy")
+set(CMAKE_OBJDUMP ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-objdump${TOOLCHAIN_BIN_SUFFIX} CACHE INTERNAL "objdump")
+
+set(CMAKE_STRIP ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-strip${TOOLCHAIN_BIN_SUFFIX} CACHE INTERNAL "strip")
+
+# Adjust the default behaviour of the FIND_XXX() commands:
+# i)    Search headers and libraries in the target environment
+# ii)   Search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Compilers like arm-none-eabi-gcc that target bare metal systems don't pass
+# CMake's compiler check, so fill in the results manually and mark the test
+# as passed:
+set(CMAKE_C_COMPILER_ID     GNU)
+set(CMAKE_COMPILER_IS_GNUCC 1)
+set(CMAKE_C_COMPILER_ID_RUN TRUE)
+set(CMAKE_C_COMPILER_FORCED TRUE)
