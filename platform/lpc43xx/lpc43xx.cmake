2abfe396771c33e17779ebd7bb5cf30e27610dc4
Nick D'Ademo <nickdademo@gmail.com>
2015-02-12 15:36:47 +1100

    Add linker scripts for redlib, newlib and newlib-nano for each hosting setting (none, nohost, semihosting). Hosting setting can be passed as a command line parameter (default is nohost).

--- platform/lpc43xx/lpc43xx.cmake
+++ platform/lpc43xx/lpc43xx.cmake
@@ -1,8 +1,8 @@
 add_definitions(
   -DCORE_M4
   -D__USE_LPCOPEN
-  -D__NEWLIB__
   -D__CODE_RED
+  -D__LPC43XX__
 
   -Wall                         # Enables all compiler warning messages
 
@@ -26,18 +26,27 @@ else()
   add_definitions(-O0 -g3 -DDEBUG)  # O0: Reduce compilation time and make debugging produce the expected results, g3: Level 3 debugging information
 endif()
 
-set(LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/platform/lpc43xx/lpc43xx.ld")
+set(LINKER_SCRIPT_DIR "${CMAKE_SOURCE_DIR}/platform/lpc43xx/ldscripts/${CLIB}")
 
-# Set specs argument based on specified C library
+# Set specs argument and linker script based on specified C library
+# newlib
+if(${CLIB} MATCHES newlib)
+  add_definitions(-D__NEWLIB__)
+  set(LINKER_SCRIPT "${LINKER_SCRIPT_DIR}/lpc43xx_newlib_${HOSTING}.ld")
+endif()
+# newlib-nano
 if(${CLIB} MATCHES newlib-nano)
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -specs=nano.specs -u _printf_float -u _scanf_float")
-elseif(${CLIB} MATCHES none)
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -specs=nosys.specs")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --specs=nano.specs -u _printf_float -u _scanf_float")
+  add_definitions(-D__NEWLIB__ -specs=nano.specs)
+  set(LINKER_SCRIPT "${LINKER_SCRIPT_DIR}/lpc43xx_newlib-nano_${HOSTING}.ld")
+# redlib
 elseif(${CLIB} MATCHES redlib)
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -specs=redlib.specs")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --specs=redlib.specs")
+  add_definitions(-D__REDLIB__ -specs=redlib.specs)
+  set(LINKER_SCRIPT "${LINKER_SCRIPT_DIR}/lpc43xx_redlib_${HOSTING}.ld")
 endif()
 
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostdlib -Xlinker --gc-sections -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mthumb -T ${LINKER_SCRIPT}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostdlib -Xlinker --gc-sections -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mthumb -T ${LINKER_SCRIPT} -L ${LINKER_SCRIPT_DIR}")
 
 set(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)
 set(OUTPUT_NAME ${CMAKE_PROJECT_NAME}.axf)
--- sources/application/main.c
+++ sources/application/main.c
@@ -1,5 +1,7 @@
 #include "chip.h"
 
+#include <cr_section_macros.h>
+
 int main(void)
 {
     while(1);
