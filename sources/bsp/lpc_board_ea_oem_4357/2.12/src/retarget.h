74ebbb17d1498c16bb0d03200f0e892bbe1d03c1
Nick D'Ademo <nickdademo@gmail.com>
2015-02-15 21:34:39 +1100

    Fix tabs in retarget.h to match rest of file.

--- sources/bsp/lpc_board_ea_oem_4357/2.12/src/retarget.h
+++ sources/bsp/lpc_board_ea_oem_4357/2.12/src/retarget.h
@@ -208,15 +208,15 @@ _STD_END
 	#define WRITEFUNC __sys_write
 	#define READFUNC __sys_readc
 #else
-    /* We are using original Redlib semihosting interface */
-    #if defined(__REDLIB__)
-	    #define WRITEFUNC __write
-	    #define READFUNC __readc
-    /* newlib / newlib-nano */
-    #else
-	    #define WRITEFUNC _write
-	    #define READFUNC _read
-    #endif
+	/* We are using original Redlib semihosting interface */
+	#if defined(__REDLIB__)
+		#define WRITEFUNC __write
+		#define READFUNC __readc
+	/* newlib / newlib-nano */
+	#else
+		#define WRITEFUNC _write
+		#define READFUNC _read
+	#endif
 #endif
 
 #if defined(DEBUG_ENABLE)
