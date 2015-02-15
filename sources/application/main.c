d7e7e628685d1e8aeda5075270280a47ef55d68e
Nick D'Ademo <nickdademo@gmail.com>
2015-02-15 15:16:43 +1100

    Add code to toggle GPIO0[8] (P1_1).

--- sources/application/main.c
+++ sources/application/main.c
@@ -2,9 +2,29 @@
 
 #include <cr_section_macros.h>
 
+// Pin Name: P1_1
+#define PORT 	0
+#define BIT		8
+
 int main(void)
 {
-    while(1);
+	int i;
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, PORT, BIT);
+
+    while(1)
+    {
+    	// P1_1-A6-BS0
+    	Chip_GPIO_SetPinState(LPC_GPIO_PORT, PORT, BIT, true);
+        for(i = 0; i < 2000000; i++)
+        {
+        	__asm__("nop");
+        }
+        Chip_GPIO_SetPinState(LPC_GPIO_PORT, PORT, BIT, false);
+        for(i = 0; i < 2000000; i++)
+        {
+        	__asm__("nop");
+        }
+    }
 
     return 0;
 }
